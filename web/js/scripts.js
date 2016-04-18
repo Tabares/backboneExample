af6371c774ba24458572e6557790672eca2428e9
Jose Tabares <jose.tabares.sotelo@accenture.com>
2016-04-18 16:12:03 -0500

    More Especific Events

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -5,7 +5,7 @@ var Magazine = Backbone.Model.extend({
   },
   initialize: function(){
     console.log("The model has been initialize");
-    this.on("change", function(){
+    this.on("change:title", function(){
       console.log("The model's data has been change");
     })
   }
