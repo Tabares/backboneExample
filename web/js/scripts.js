bec869dd0293b309dae1f8ef713a97711361b351
Jose Tabares <jose.tabares.sotelo@accenture.com>
2016-04-18 15:28:26 -0500

    Basic Model

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -18,5 +18,21 @@ var Magazine1 = Backbone.Model.extend({
 });
 
 var magazine2 = new Magazine1();
-
 console.log(JSON.stringify(magazine2));
+
+var magazine3 = new Magazine1();
+console.log(magazine3.get("pubDate"));
+
+var magazine4 = new Magazine1({
+  title:"Screaming Zebras"
+});
+console.log(magazine4.get("title"));
+
+var magazine4 = new Magazine1({
+  title:"Screaming Zebras"
+});
+console.log("Magazine 1 title: " + magazine4.get("title") );
+
+var magazine5 = new Magazine1();
+magazine5.set("title", "Sobrero Monthly");
+console.log("Magazine 1 title: " + magazine5.get("title") );
